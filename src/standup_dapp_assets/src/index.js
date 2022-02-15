fe5bf02f6f7103767cc79a21e188ed5006230ea3
Sue Ann Kim <sueannkim@SueAnnKimsMBP.attlocal.net>
2022-02-15 11:34:25 -0800

    Initial commit.

---
+++ src/standup_dapp_assets/src/index.js
@@ -0,0 +1,19 @@
+import { standup_dapp } from "../../declarations/standup_dapp";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await standup_dapp.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
